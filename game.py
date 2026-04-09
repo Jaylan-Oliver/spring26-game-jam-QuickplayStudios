3124c35877dd25c34e4d8738d5bf364403a65bb9
Abi Mondido <abimondido@gmail.com>
2026-04-09 01:44:27 -0400

    added sprite facing l/r

--- game.py
+++ game.py
@@ -15,6 +15,7 @@ class Player:
         self.y = 50
         self.surface = pyg.transform.scale(img, (img.get_width()*2, img.get_height()*2)) # sprite surface itself
         self.hitbox = pyg.Rect(self.x, self.y, self.surface.get_width(), self.surface.get_height()) # moves based on position
+        self.l_facing = True
         
     def setpos(self, x, y):
         self.x = x
@@ -24,6 +25,10 @@ class Player:
     def updatepos(self):
         self.hitbox.x = self.x
         self.hitbox.y = self.y
+    
+    def turn(self):
+        self.surface = pyg.transform.flip(self.surface, True, False)
+        self.l_facing = not self.l_facing
 
 # Assets & Objects
 cake_img = pyg.image.load('./assets/cake.png').convert_alpha()
@@ -48,8 +53,12 @@ while running:
     keys = pyg.key.get_pressed()
     if keys [pyg.K_LEFT]:
         cake.x -= velocity
+        if (not cake.l_facing):
+            cake.turn()
     if keys [pyg.K_RIGHT]:
         cake.x += velocity
+        if(cake.l_facing):
+            cake.turn()
     if keys [pyg.K_UP]:
         cake.y -= velocity
     if keys [pyg.K_DOWN]:
